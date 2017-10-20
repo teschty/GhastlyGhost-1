8b0fa652f3bcf5b1c4b8d069bcd9c7f87f6ef214
blhowell <bhowell@aggienetwork.com>
2017-10-20 18:55:20 -0500

    changes. rot13

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+#!/usr/bin/python3
+import argparse
+import string
+
+parser = argparse.ArgumentParser(description='ROT13')
+parser.add_argument('string', metavar='string', type=str, nargs='+', help='rot 13 for alphabet only chars.')
+# parser.add_argument('-l', metavar='lowercase', type=str, nargs='+', help='make lowercase.')
+# parser.add_argument('-u', metavar='uppercase', type=str, nargs='+', help='make uppercase.')
+
+args = parser.parse_args()
+text = args.string
+
+alphabet = string.ascii_lowercase
+print(text)
+print(alphabet)
