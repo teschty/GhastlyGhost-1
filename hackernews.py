c7dc2d3d97eb15f1375304a63db2bb1165585655
Tyler Tesch <tytesch@gmail.com>
2017-10-20 21:17:31 -0500

    UPDATE

--- hackernews.py
+++ hackernews.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 html = urlopen("https://news.ycombinator.com/").read().decode('utf-8')
+soup = BeautifulSoup(html)
 
 # IF ONLY SOMEONE COULD COMPLETE THIS :(
 # AND MAKE IT DISPLAY THE TOP 10 TITLES FROM HACKER NEWS
