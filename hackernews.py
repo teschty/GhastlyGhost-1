227cae6cc38eba9d73b8a368f97fdcd514528650
Tyler Tesch <tytesch@gmail.com>
2017-10-20 21:14:23 -0500

    QUEST

---
+++ hackernews.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+html = urlopen("https://news.ycombinator.com/")
+
+# IF ONLY SOMEONE COULD COMPLETE THIS :(
+# AND MAKE IT DISPLAY THE TOP 10 TITLES FROM HACKER NEWS
