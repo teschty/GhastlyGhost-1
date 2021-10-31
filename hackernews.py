5bdfc9d74c8ad0016d4797c9109d8aa64356e600
Tyler Tesch <teschty@users.noreply.github.com>
2021-10-31 08:57:56 -0500

    Use zip for iterating over story/counter

--- hackernews.py
+++ hackernews.py
@@ -7,10 +7,8 @@ data = requests.get("https://news.ycombinator.com/").text
 soup = BeautifulSoup(data, "html5lib")
 
 counter = 0
-for story in soup.find_all("a", class_="storylink"):
-    if counter < 10:
-        counter += 1
-        print("~~~~~~~")
-        print("{0}. {1.string}".format(counter, story))
+for story, counter in zip(soup.find_all("a", class_="storylink"), range(10)):
+    print("~~~~~~~")
+    print("{0}. {1.string}".format(counter, story))
 
 print("~~~~~~~")
